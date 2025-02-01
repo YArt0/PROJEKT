9196ac2e4fe3d71b17aca5407e86a5a1d87788c6
YArt0 <damian0202vp@gmail.com>
2025-02-01 20:24:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// Tworzymy obiekt do odczytu tekstu
 let speech = new SpeechSynthesisUtterance();
 
 // Funkcja do odczytu tekstu danego rozdziału
 function readSection(sectionId) {
     const sectionText = document.getElementById(sectionId).innerText;
     speech.text = sectionText;
-    speech.lang = 'pl-PL';
-    speech.rate = 1; // Standardowe tempo
-    speech.volume = 1; // Standardowa głośność
+    speech.lang = 'pl-PL'; // Ustawienie języka na polski
+    speech.rate = 1; // Tempo odczytu (1 to standardowe)
+    speech.volume = 1; // Głośność (1 to maksymalna głośność)
+    
+    // Rozpoczynamy odczyt
     speechSynthesis.speak(speech);
 }
 
